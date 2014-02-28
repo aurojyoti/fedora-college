dc261b25778a023b6c8d7c788beb8a39572c70fd
hammad haleem <hammadhaleem@gmail.com>
2014-02-28 20:59:36 +0530

    Added more routes

--- fedora_college/modules/home/views.py
+++ fedora_college/modules/home/views.py
@@ -4,8 +4,22 @@ from flask import Blueprint, render_template
 bundle = Blueprint('home', __name__, template_folder='templates')
 
 
-@bundle.route('/', methods=['GET', 'POST'])
-@bundle.route('/index', methods=['GET', 'POST'])
-@bundle.route('/home', methods=['GET', 'POST'])
+@bundle.route('/',      methods=['GET', 'POST'])
+@bundle.route('/index/', methods=['GET', 'POST'])
+@bundle.route('/home/',  methods=['GET', 'POST'])
+
 def index():
-    return render_template('home/index.html')
+    return render_template('home/index.html', title = 'Home',content="Home page")
+
+
+@bundle.route('/screencasts/',  methods=['GET', 'POST'])
+def screencasts():
+    return render_template('home/index.html', title = 'Screen Casts',content="Fedora Screencasts")
+
+
+
+@bundle.route('/about/', methods=['GET', 'POST'])
+def about():
+    return render_template('home/index.html', title = 'About',content="About Us")
+
+
